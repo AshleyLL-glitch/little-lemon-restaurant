d5c3fa223bd346afefa30fae67b46d65e2acfb7c
AshleyLL-glitch <allewis169@gmail.com>
2025-01-18 14:18:41 -0700

    added meta tags

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2025 Little Lemon. All rights reserved.</p>
+      <ul>
+        <li><a href="/privacy-policy">Privacy Policy</a></li>
+        <li><a href="/terms-of-service">Terms of Service</a></li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
